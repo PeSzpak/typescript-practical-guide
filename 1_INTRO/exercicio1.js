15a77ead2c2c1aff0885dcaf128d3ffc5b05fc72
Pedro Szpak <dropszpalves@gmail.com>
2025-06-16 11:17:11 -0300

    first commit about intro and first module

---
+++ 1_INTRO/exercicio1.js
@@ -0,0 +1,6 @@
+var A = 10;
+var B = 5;
+function soma(a, b) {
+    return a + b;
+}
+console.log(soma(A, B)); // 15
---
+++ 1_INTRO/index.js
@@ -0,0 +1,9 @@
+var firstName = 'Pedro';
+var anotherName = 'Szpak';
+var x = true;
+function greeting(name) {
+    console.log("Óla " + name);
+}
+greeting(firstName);
+greeting(anotherName);
+greeting(x);
