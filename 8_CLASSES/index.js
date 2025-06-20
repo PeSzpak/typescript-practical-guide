5d062629bef7b1d8bbabbd71692002295b63b87b
Pedro Szpak <dropszpalves@gmail.com>
2025-06-20 10:41:41 -0300

    feat: add class fields

---
+++ 8_CLASSES/index.js
@@ -0,0 +1,8 @@
+"use strict";
+//Class property 
+class User {
+}
+const Pak = new User();
+console.log(Pak);
+Pak.name = "Pedo Szpak";
+console.log(Pak);
---
+++ 8_CLASSES/index.ts
@@ -0,0 +1,15 @@
+  //Class property 
+  class User {
+    name!: string
+    age!: number
+  }
+
+  const Pak = new User()
+
+  console.log(Pak)
+
+  Pak.name = "Pedo Szpak";
+
+  console.log (Pak)
+
+  
