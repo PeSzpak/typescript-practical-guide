205fdcad8f03bb36f4b0214a95a9a2ff93d8cf25
Pedro Szpak <dropszpalves@gmail.com>
2025-06-20 15:36:45 -0300

    feat: add file import support

---
+++ 9_MODULES/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+//file import 
+//rode do node NOMEDOARQUIVO.js 
+const greet_1 = __importDefault(require("./greet"));
+(0, greet_1.default)();
