defd8f9f2d7dec9c23f74e0a7bcca5381f428ad1
Pedro Szpak <dropszpalves@gmail.com>
2025-06-24 09:53:13 -0300

    feat: consume typed Context API

--- 11_react_ts/src/App.tsx
+++ 11_react_ts/src/App.tsx
@@ -1,8 +1,9 @@
 import React, { createContext } from 'react';
-import FirstComponent from './components/firtsComponent';
-import SecondyComponent from './components/secondyComponent';
+import FirstComponent from './components/FirtsComponent';
+import SecondyComponent from './components/SecondyComponent';
 import Destructuring, { Category } from './components/Destructuring';
 import State from './components/State';
+import Context from './components/Context';
 //import { createContext } from 'vm';   Usado para rodar codigos n confiaveis.
 
 type textOrNull = string | null
@@ -14,7 +15,7 @@ interface IAppContext {
   projects: number
 }
 
-export const AppContext = createContext< IAppContext | null> (null)
+export const AppContext = createContext<IAppContext | null>(null)
 
 function App() {
 
@@ -33,59 +34,60 @@ function App() {
   //mySecondText = "opa"
 
   const testandoFixed: fixed = "Isso"
- // const testandoFixed2: fixed = "Isdsdasasdaso" erro pois só aceita valores ja declarados na tipagem 
+  // const testandoFixed2: fixed = "Isdsdasasdaso" erro pois só aceita valores ja declarados na tipagem 
 
- const contextValue: IAppContext = {
-  language: "JavaScript",
-  framework: "Express",
-  projects: 5,
- }
+  const contextValue: IAppContext = {
+    language: "JavaScript",
+    framework: "Express",
+    projects: 5,
+  }
 
   return (
     <AppContext.Provider value={contextValue}>
-    <div className="App">
-      <h1>TypeScript com React</h1>
+      <div className="App">
+        <h1>TypeScript com React</h1>
 
-      <hr />
-      <h2>Nome: {name}</h2>
-      <p>Idade: {age}</p>
-      {isStuding && (
-        <div>
-          <p>Está Estudando!</p>
-        </div>
-      )}
-      <hr />
+        <hr />
+        <h2>Nome: {name}</h2>
+        <p>Idade: {age}</p>
+        {isStuding && (
+          <div>
+            <p>Está Estudando!</p>
+          </div>
+        )}
+        <hr />
 
-      <h3>{userGreeting(name)}</h3>
+        <h3>{userGreeting(name)}</h3>
 
-      <hr />
-      <FirstComponent />
-      <SecondyComponent name="Segundo" />
-      <Destructuring
-        title="primeiro post"
-        content="Algum conteudo"
-        commentsQty={153}
-        tags={['ts', 'js']}
-        category={Category.TS}
-      />
-      <Destructuring
-        title="Segundo post"
-        content="Mais Algum conteudo"
-        commentsQty={444}
-        tags={['python']}
-        category={Category.P}
-      />
-      <hr />
-      
-      <State />
+        <hr />
+        <FirstComponent />
+        <SecondyComponent name="Segundo" />
+        <Destructuring
+          title="primeiro post"
+          content="Algum conteudo"
+          commentsQty={153}
+          tags={['ts', 'js']}
+          category={Category.TS}
+        />
+        <Destructuring
+          title="Segundo post"
+          content="Mais Algum conteudo"
+          commentsQty={444}
+          tags={['python']}
+          category={Category.P}
+        />
+        <hr />
 
-      {myText &&
-        <p>Tem texto na variavel</p>
-      }
-      {mySecondText &&
-        <p>tem texto na variavel</p>
-      }
-    </div>
+        <State />
+
+        {myText &&
+          <p>Tem texto na variavel</p>
+        }
+        {mySecondText &&
+          <p>tem texto na variavel</p>
+        }
+        <Context />
+      </div>
     </AppContext.Provider>
   );
 }
