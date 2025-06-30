947254c66532ea749995a6a38c87c2cfc3d43ec4
Pedro Szpak <dropszpalves@gmail.com>
2025-06-30 11:40:09 -0300

    feat: initialize Express project and add basic POST route with API testing instructions

---
+++ 12_EXPRESS/src/app.ts
@@ -0,0 +1,20 @@
+//console.log("Express + TS")
+
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.get("/", (req, res) => {
+    return res.send("Hello Express!");
+})
+
+app.post("/api/product", (req, res) => {
+    console.log(req.body)
+
+    return res.send("Produto adicionado!")
+})
+
+app.listen(3000, () => {
+    console.log ("Aplicação de TS + express está funcionando!");
+})
