e6d575442ed08a10e09a35951b75db3ab38085ac
Pedro Szpak <dropszpalves@gmail.com>
2025-07-01 11:09:09 -0300

    feat: implement complex routes with multiple parameters and query support

--- 12_EXPRESS/src/app.ts
+++ 12_EXPRESS/src/app.ts
@@ -55,6 +55,14 @@ app.get("/api/product/:id", (req: Request, res:Response) => {
 }
 })
 
+app.get("/api/product/:id/review/:reviewId", (req: Request, res:Response) => {
+        console.log(req.params)
+        const productId = req.params.id
+        const reviewId = req.params.reviewId
+
+        res.send(`Acessando a review ${reviewId} do produto ${productId}`)
+})
+
 app.listen(3000, () => {
     console.log ("Aplicação de TS + express está funcionando!");
 })
