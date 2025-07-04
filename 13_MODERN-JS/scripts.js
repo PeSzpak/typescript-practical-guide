e4b4e1c2c2db3dde11379334230d78409328fe61
Pedro Szpak <dropszpalves@gmail.com>
2025-07-04 11:47:16 -0300

    docs: add notes and examples on arrow functions

--- 13_MODERN-JS/scripts.js
+++ 13_MODERN-JS/scripts.js
@@ -28,6 +28,44 @@ const name = "Pedro"
 logName()
 console.log(name)
 
+//Arrow function 
+const sum = function sum(a, b) {
+    return a + b 
+}
+
+const arrowSum = (a, b) => a + b
+
+console.log(sum(5,5))
+console.log(arrowSum(5,5))
+
+const greeting = (name) => {
+    if(name) {
+        return 'Olá ' + name + '!'
+    }else {
+        return 'Olá'
+    }
+}
+
+console.log(greeting("Pedro"))
+console.log(greeting())
 
 
+const testeArrow = () => console.log('testou')
+testeArrow()
 
+const user = {
+name: "Ayrton",
+sayUrseName() {
+    var self = this
+    setTimeout(function() {
+        console.log(self)
+        console.log("Username " + self.name)
+    }, 500)
+},
+sayUserNameArrow() {
+    setTimeout(() => {
+        console.log("Username " + this.name)
+    },1000)
+}
+}
+user.sayUrseName()
