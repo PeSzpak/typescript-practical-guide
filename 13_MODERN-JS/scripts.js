e5a8a537fe223769a08b13e6bfc7cd952fa9970b
Pedro Szpak <dropszpalves@gmail.com>
2025-07-04 11:59:46 -0300

    docs: add notes and examples on Array.prototype.filter

--- 13_MODERN-JS/scripts.js
+++ 13_MODERN-JS/scripts.js
@@ -69,3 +69,28 @@ sayUserNameArrow() {
 }
 }
 user.sayUrseName()
+
+//filter 
+const arr = [1, 2, 3, 4, 5]
+
+console.log(arr)
+
+const highNumbers = arr.filter((n) => {
+    if (n >= 3) {
+        return n 
+    }
+})
+
+console.log(highNumbers)
+
+const users = [
+    {name: 'matheus', available: false},
+    {name: 'Junior', available: false},
+    {name: 'Pedro', available: true},
+    {name: 'Thiago', available: true}
+]
+
+const availableUsers = users.filter((user) => user.available)
+const notAvailableUsers = users.filter((user) => !user.available)
+console.log(availableUsers)
+console.log(notAvailableUsers)
