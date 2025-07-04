410761e79bbb0dc973f291a780d2c7528c362aa9
Pedro Szpak <dropszpalves@gmail.com>
2025-07-04 11:30:26 -0300

    Feat: add examples about ES6 of variable declarations

---
+++ 13_MODERN-JS/scripts.js
@@ -0,0 +1,33 @@
+//Var let e const
+var x = 10
+var y = 15
+
+if (y > 10) {
+    var x = 5
+    console.log(x)
+}
+
+console.log(x)
+
+let a = 10
+let b = 15
+
+if (b = 10) {
+    let a = 5
+    console.log(b)
+}
+
+console.log(a)
+
+function logName() {
+    const name = "Isabelle"
+    console.log(name)
+}
+const name = "Pedro"
+
+logName()
+console.log(name)
+
+
+
+
