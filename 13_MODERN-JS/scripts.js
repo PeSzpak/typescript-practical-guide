e5d3bbcfde5b1db8f6cd56cea1a5828b4da241de
Pedro Szpak <dropszpalves@gmail.com>
2025-07-07 09:30:18 -0300

    feat: apply destructuring to extract values from arrays and objects

--- 13_MODERN-JS/scripts.js
+++ 13_MODERN-JS/scripts.js
@@ -116,3 +116,22 @@ const age = 30
 
 console.log(`O nome do usuario é ${userName} e ele tem ${age} anos.`)
 console.log('O nome do usuario é ' + userName + 'e ele tem ' + age + ' anos.')
+
+//destructuring 
+const fruits = ["pitaya", "maracujá", "melancia"]
+
+const [f1, f2, f3] = fruits
+
+console.log(f1)
+console.log(f3)
+
+const productDetails = {
+    name: "Mouse",
+    price: 39.99,
+    category: "Periféricos",
+    color: "black" 
+}
+
+const  {name: productName, price , category: productCategory, color} = productDetails
+
+console.log(`O nome do produto é ${productName}, custa R$${price}, pertence a categoria ${productCategory} e é da cor ${color}`)
